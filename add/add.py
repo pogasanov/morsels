33cb5163542c131d0dc9c4ea809d239a16c0ddf6
pavel <pogasanov@gmail.com>
2019-09-26 13:15:15 +0700

    use none for matrices size validation

--- add/add.py
+++ add/add.py
@@ -1,15 +1,13 @@
 from itertools import zip_longest
 
-fillvalue = object()
-
 
 def add(*args):
     try:
         return sum(args)
     except TypeError:
         summed_result = []
-        for zipped in zip_longest(*args, fillvalue=fillvalue):
-            if fillvalue in zipped:
+        for zipped in zip_longest(*args):
+            if None in zipped:
                 raise ValueError("Given matrices are not the same size.")
             summed_result.append(add(*zipped))
         return summed_result
