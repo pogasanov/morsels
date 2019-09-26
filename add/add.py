945819d33412ab1689524d0f017c6bbcc4071d18
pavel <pogasanov@gmail.com>
2019-09-26 13:00:59 +0700

    use recursion to calculate sum

--- add/add.py
+++ add/add.py
@@ -1,16 +1,15 @@
 from itertools import zip_longest
 
+fillvalue = object()
+
 
 def add(*args):
-    fillvalue = object()
-    response = []
-    for zipped in zip_longest(*args, fillvalue=fillvalue):
-        if fillvalue in zipped:
-            raise ValueError()
-        inp = []
-        for items in zip_longest(*zipped, fillvalue=fillvalue):
-            if fillvalue in items:
-                raise ValueError()
-            inp.append(sum(items))
-        response.append(inp)
-    return response
+    try:
+        return sum(args)
+    except TypeError:
+        summed_result = []
+        for zipped in zip_longest(*args, fillvalue=fillvalue):
+            if fillvalue in zipped:
+                raise ValueError("Given matrices are not the same size.")
+            summed_result.append(add(*zipped))
+        return summed_result
