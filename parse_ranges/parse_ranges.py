be54e52dcc3bc1ef6aba75ff24be8840b63d0875
pavel <pogasanov@gmail.com>
2019-10-15 12:16:03 +0700

    minor refactoring

--- parse_ranges/parse_ranges.py
+++ parse_ranges/parse_ranges.py
@@ -2,10 +2,11 @@ def parse_ranges(input_string):
     for range_of_numbers in input_string.split(','):
         try:
             a, b = range_of_numbers.split('-')
+        except ValueError:
+            yield int(range_of_numbers)
+        else:
             if b == '>exit':
                 yield int(a)
             else:
                 for x in range(int(a), int(b) + 1):
                     yield x
-        except ValueError:
-            yield int(range_of_numbers)
