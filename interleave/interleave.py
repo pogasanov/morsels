fd91af771afeac48cd3a7556d2054f92a6a8aad2
Pavel Gasanov <pavel@seriescode.com>
2019-12-16 23:56:21 +0700

    interleave complete

---
+++ interleave/interleave.py
@@ -0,0 +1,8 @@
+from itertools import zip_longest
+
+sentinel = object()
+
+
+def interleave(*el):
+    for zipped in zip_longest(*el, fillvalue=sentinel):
+        yield from (item for item in zipped if item is not sentinel)
