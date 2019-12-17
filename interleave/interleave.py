e7fbc7a01cb586b1934715c653e9f41125c705a9
Pavel Gasanov <pavel@seriescode.com>
2019-12-17 11:52:47 +0700

    formatting

--- interleave/interleave.py
+++ interleave/interleave.py
@@ -6,6 +6,7 @@ sentinel = object()
 def interleave(*el):
     for zipped in zip_longest(*el, fillvalue=sentinel):
         yield from (
-            item for item in zipped
+            item
+            for item in zipped
             if item is not sentinel
         )
